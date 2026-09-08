1686a7476538f2846cda4bc9ccd308f8601b903b
Sanket0248 <Sanketgarg2004@gmail.com>
2026-09-09 00:56:30 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] memo = new int[nums.length];
+        memo[0] = nums[0];
+        memo[1] =  Math.max(nums[0],nums[1]);
+        return robFrom(nums.length-1,nums,memo);
+
+    }
+
+    public int robFrom(int i, int[] nums, int[] memo){
+        // if(i >= nums.length) return 0;
+        if(i == 0) return memo[0];
+        if(i == 1) return memo[1];
+
+        if(memo[i] != 0) return memo[i];
+
+        memo[i] = Math.max(robFrom(i-1,nums,memo), robFrom(i-2,nums,memo) + nums[i]);
+
+        return memo[i];
+    }
+}
