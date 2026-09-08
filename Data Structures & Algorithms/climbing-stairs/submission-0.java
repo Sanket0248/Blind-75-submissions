054e307f6840b14c31287728015cb14cd6bb6669
Sanket0248 <Sanketgarg2004@gmail.com>
2026-09-08 19:17:47 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        if(dp[n] != 0) return dp[n];
+
+        return dp[n-1] + dp[n-2];
+    }
+}
